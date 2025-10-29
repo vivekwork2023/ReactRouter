c401de3a1f43a5d0b522c5217ef46282ceaf2ce9
Vivek Reddy Kancharla <vkanchar@fastenal.com>
2025-10-29 11:18:49 +0530

    The basic stuff

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,24 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 
 const App = () => {
-  return ( <div>
+  return (<div>
     Hello there
-    <Navbar/>
+    <Navbar />
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* The default page that will be loaded, becuase it is checking the url and checking which of these paths are present */}
 
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/products' element={<Products/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
-    </Routes>
+        <Route path='/products' element={<Products />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
     {/* so from the main header when the web loads, this page will be shown as the first page.
     so when, we add any of these paths to the main URL,then it will redirect. 
     Like http://localhost:5173 -> http://localhost:5173/contact will open component Contact along with the Navbar */}
-    
-  </div> );
+
+  </div>);
 }
- 
+
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import logo from '../assets/vite.svg'
+import React from 'react';
+import logo from '../assets/vite.svg';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
         <img src={logo} alt='A log page' className='logo-img'/>
         <ul>
+          <Link to={'/about'}>
             <li>About</li>
+          </Link>
+          <Link to={'/contact'}>
             <li>Contact</li>
+          </Link>
+          <Link to={'/'}>
             <li>Home</li>
+          </Link>
+          <Link to={'/products'}>
             <li>Products</li>
+          </Link>
         </ul>
+        <button>Get Started</button>
     </div>
   )
 }
