3dbb3688ea741f5ac824862b73b19922c3960bef
Vivek Reddy Kancharla <vkanchar@fastenal.com>
2025-10-29 22:57:22 +0530

    use navigate working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import logo from '../assets/vite.svg';
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className='navbar'>
         <img src={logo} alt='A log page' className='logo-img'/>
@@ -21,7 +23,7 @@ const Navbar = () => {
             <li>Products</li>
           </NavLink>
         </ul>
-        <button>Get Started</button>
+        <button onClick={() => { navigate('/about', {replace:true}) } }>Get Started</button>
     </div>
   )
 }
